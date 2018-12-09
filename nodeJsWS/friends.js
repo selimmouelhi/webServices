7df9ac031ebbc4646ea8a44aa17cc8ce2ac10d4a
selimmouelhi <slmmlh30@gmail.Com>
2018-12-09 23:23:05 +0100

    friends web services done

--- nodeJsWS/friends.js
+++ nodeJsWS/friends.js
@@ -18,9 +18,9 @@ app.use(function (req, res, next) {
 
 
 //intializing server
-var server = app.listen(3003, function(){
+var server = app.listen(3005, function(){
 
-    console.log("app is listening on port 3003")
+    console.log("app is listening on port 3005")
 
 
 })
@@ -70,5 +70,82 @@ app.get("/web_services/friends",function(req,res){
     
     })
     })
+
+    app.post("/web_services/addfriend",function(req,res){
+
+
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root', 
+            database:'espritlaunch',
+            port: '3306'
+        
+            
+        
+          })
+          
+          
+            
+          connection.query("INSERT INTO friends(id_friend, id_user) VALUES (?, ?)", [
+            req.body.id_friend,
+            req.body.id_user
+            ], (err, rows, fields) => {
+              
+            if(err){
+                console.log("error while fteching query")
+                res.send(err)
+            }
+            else{
+                res.send(req.params.nom)
+                console.log("done")
+            }
+            
+          
+        
+        })
+        })
+
+
+
+        //delete user by id 
+
+        app.delete("/web_services/deletefriend",function(req,res){
+
+
+            const connection = mysql.createConnection({
+                host: 'localhost',
+                user: 'root',
+                password: 'root', 
+                database:'espritlaunch',
+                port: '3306'
+            
+                
+            
+              })
+              
+              
+                
+              connection.query("DELETE FROM friends WHERE id_user=? AND  id_friend=?", [
+                req.body.id_user,
+                req.body.id_friend
+                ], (err, rows, fields) => {
+                  
+                if(err){
+                    console.log("error while fteching query")
+                    res.send(err)
+                }
+                else{
+                    res.send(req.params.nom)
+                    console.log("done")
+                }
+                
+              
+            
+            })
+            })
+            
+        
+    //SELECT * FROM user INNER JOIN friends ON user.id=friends.id_user And friends.id_user="f_2308446465863370"
 
 
